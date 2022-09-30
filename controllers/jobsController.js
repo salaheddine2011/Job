27a4ca0beb50188813ba1a50623c573c09850a68
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-09-30 21:55:14 +0100

    Fixing the number of page limit to 10

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -50,7 +50,7 @@ const getAllJobs = async (req, res) => {
     }
     
     const page =Number(req.query.page)||1;
-    const limit=Number(req.query.limit)||2;
+    const limit=Number(req.query.limit)||10;
 
     const skip=(page-1)*limit
     result=result.skip(skip).limit(limit)
