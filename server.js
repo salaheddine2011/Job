fb0368ac34139d2e4d092bb66bc4e5c1872726a2
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-10-01 16:14:30 +0100

    Security Packages - HELMET, XSS-CLEAN, EXPRESS-MONGO-SANITIZE

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import morgan from "morgan";
 import {dirname} from 'path'
 import { fileURLToPath } from "url";
 import path from 'path'
+
+import helmet from "helmet";
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
 //db and authenticateUser
 import connectDB from "./db/connect.js";
 //routers
@@ -25,8 +29,13 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 const __dirname=dirname(fileURLToPath(import.meta.url))
+//only when ready to deploy
 app.use(express.static(path.resolve(__dirname,'./client/build')))
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+
 
 
 
