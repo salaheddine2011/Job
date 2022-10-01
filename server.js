991b357596501e54e6923a1dea0d93b280faa9e6
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-10-01 16:04:50 +0100

    Build Front-End Application

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 import 'express-async-errors'
 import morgan from "morgan";
+
+import {dirname} from 'path'
+import { fileURLToPath } from "url";
+import path from 'path'
 //db and authenticateUser
 import connectDB from "./db/connect.js";
 //routers
@@ -19,7 +23,12 @@ import authenticateUser from './middleware/auth.js'
 if(process.env.NODE_ENV !== 'production'){
     app.use(morgan('dev'))
 }
+
+const __dirname=dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname,'./client/build')))
 app.use(express.json())
+
+
 
 app.get('/',(req,res)=>{
 res.json({msg:'Welcome'})
@@ -31,6 +40,10 @@ app.get('/api/v1',(req,res)=>{
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',authenticateUser,jobsRouter)
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
